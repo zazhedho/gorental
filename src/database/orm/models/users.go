6e39367cd71f5618194e26cc9ed525c7407900cc
zazhedho <zaiduszhuhur1@gmail.com>
2022-09-15 01:28:18 +0700

    move file

--- src/database/orm/models/users.go
+++ src/database/orm/models/users.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	UserId      string    `gorm:"primaryKey" json:"id"`
+	UserId      uint      `gorm:"primaryKey" json:"id"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
 	Password    string    `json:"password"`
